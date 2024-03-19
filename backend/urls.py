027572f7ce8ed8cb4e15490e77d51634bb504545
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-03-18 23:29:39 -0500

    build: :wrench: change name of directory

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,11 @@
 """
-URL configuration for ican_project project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from ican_project import views
+    1. Add an import:  from backend import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
