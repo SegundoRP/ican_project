9bd59b77b684bba252a6d8ac54a0839835e001cf
Ricardo2930 <rhuaytan@gmail.com>
2024-04-30 13:40:01 -0500

    feat: :sparkles: The fifth experience component was created and the image was added for the component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Introduccion from "@/components/Introduccion";
 import Servicios from "@/components/Servicios";
 import Nosotros from "@/components/Nosotros";
+import Experiencia from "@/components/Experiencia";
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       <Introduccion />
       <Servicios />
       <Nosotros />
+      <Experiencia />
+
     </main>
   );
 }
