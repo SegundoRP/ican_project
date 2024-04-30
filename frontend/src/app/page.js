0dbdd9bd5b93c7be5739d334868f13cd08ecefc3
Ricardo2930 <rhuaytan@gmail.com>
2024-04-30 13:50:00 -0500

    feat: :sparkles: The seventh footer component was created and the image for the component was added

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,6 +4,8 @@ import Servicios from "@/components/Servicios";
 import Nosotros from "@/components/Nosotros";
 import Experiencia from "@/components/Experiencia";
 import Testimonios from "@/components/Testimonios";
+import Footer from "@/components/Footer";
+
 
 
 export default function Home() {
@@ -15,7 +17,7 @@ export default function Home() {
       <Nosotros />
       <Experiencia />
       <Testimonios />
-
+      <Footer />
     </main>
   );
 }
