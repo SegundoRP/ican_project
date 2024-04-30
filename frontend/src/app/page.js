220b3ec69540b9a5bbc27ca07b5b248e248bf7d6
Ricardo2930 <rhuaytan@gmail.com>
2024-04-30 13:36:33 -0500

    feat: :sparkles: The third services component was created

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Introduccion from "@/components/Introduccion";
+import Servicios from "@/components/Servicios";
 
 export default function Home() {
   return (
     <main className="">
       <Navbar /> 
       <Introduccion />
+      <Servicios />
     </main>
   );
 }
