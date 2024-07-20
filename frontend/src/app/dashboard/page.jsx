ee031dea3d746d420faeadde01d5e560c4c8d0f1
Ricardo2930 <rhuaytan@gmail.com>
2024-07-20 11:10:49 -0500

    feat: :sparkles: Se agrego la seccion de Bienvenido en la pagina principal del Dashboard

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -1,10 +1,26 @@
+import { Avatar, Datepicker } from "flowbite-react";
 
 
-export default function page() {
+export default function pageDashboard() {
   return (
-    <div className="p-10 bg-slate-300">
-      Dashboard
-    </div>
+    <main className="p-4 bg-slate-300">
+      <section className="my-2 sm:my-4">
+        <div className="grid place-content-center sm:flex sm:items-center sm:gap-4">
+          <Avatar img="https://unavatar.io/segundorp" rounded size="lg" />
+          <div className="text-center space-y-2">
+            <h2 className="text-3xl sm:text-4xl font-bold">Bienvenido Segundo</h2>
+            <Datepicker
+              className="flex justify-center"
+              language="es-ES"
+              labelTodayButton="Hoy"
+              labelClearButton="Limpar"
+              autoHide={false}
+              weekStart={1}
+            />
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
