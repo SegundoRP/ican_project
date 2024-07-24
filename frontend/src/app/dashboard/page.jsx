d8e16ed9c80e920a6566470d0ab39ed1f0c08bec
Ricardo2930 <rhuaytan@gmail.com>
2024-07-24 11:24:37 -0500

    feat: :sparkles: Se creo y agrego el componente Bienvenido a la pagina principal del Dashboard

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -1,26 +1,11 @@
+import Bienvenido from "@/components/dashboard/Bienvenido";
 import Cards from "@/components/dashboard/cards/Cards";
-import { Avatar, Datepicker } from "flowbite-react";
 
 
 export default function pageDashboard() {
   return (
     <main className="p-4 bg-slate-300">
-      <section className="my-2 sm:my-4">
-        <div className="grid place-content-center sm:flex sm:items-center sm:gap-4">
-          <Avatar img="https://unavatar.io/segundorp" rounded size="lg" />
-          <div className="text-center space-y-2">
-            <h2 className="text-3xl sm:text-4xl font-bold">Bienvenido Segundo</h2>
-            <Datepicker
-              className="flex justify-center"
-              language="es-ES"
-              labelTodayButton="Hoy"
-              labelClearButton="Limpar"
-              autoHide={false}
-              weekStart={1}
-            />
-          </div>
-        </div>
-      </section>
+      <Bienvenido/>
       <Cards/>
     </main>
   )
