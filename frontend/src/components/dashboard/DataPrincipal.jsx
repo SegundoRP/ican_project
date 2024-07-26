1f0dfb8333a4750d45970a87129ff28daab74b9a
Ricardo2930 <rhuaytan@gmail.com>
2024-07-26 09:13:34 -0500

    feat: :sparkles: Se actualizo el componente DataPrincipalOrdenes

--- frontend/src/components/dashboard/DataPrincipal.jsx
+++ frontend/src/components/dashboard/DataPrincipal.jsx
@@ -4,7 +4,7 @@ import DataPrincipalRepartidevs from "./tablas/DataPrincipalRepartidevs";
 
 export default function DataPrincipal() {
   return (
-    <section className="flex flex-col lg:grid grid-cols-4 gap-5 mb-7">
+    <section className="flex flex-col lg:grid grid-cols-4 gap-5">
     <div className="bg-white p-5 rounded-xl lg:col-span-3">
       <h2 className="text-xl font-bold text-gray-900 border-b-4 border-yellow-200 mb-7 inline-block">
         Ordenes
