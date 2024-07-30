5adb405c91e7b1b36f5b4dfdbb40aaec3a2a4d9b
Ricardo2930 <rhuaytan@gmail.com>
2024-07-30 15:16:57 -0500

    feat: :sparkles: Se actualizo la pagina de ordenes con su tablas, modal y cards

--- frontend/src/app/dashboard/ordenes/page.jsx
+++ frontend/src/app/dashboard/ordenes/page.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import CardsOrdenes from "@/components/dashboard/cards/CardsOrdenes";
+import DataPrincipalOrdenes from "@/components/dashboard/tablas/DataPrincipalOrdenes";
+import React from "react";
 
-export default function page() {
+export default function pageOrdenes() {
   return (
-    <div className="p-10 bg-slate-300">
-      Ordenes
-    </div>
-  )
+    <main className="mt-28 p-4 sm:p-6 bg-gray-200">
+      <section className="flex flex-col gap-5 lg:grid xl:grid-cols-7">
+        <article className="bg-white p-5 rounded-xl lg:grid xl:col-span-6">
+          <div>
+            <header className="flex justify-center sm:justify-start text-3xl font-bold text-gray-900 border-b-4 border-yellow-200 mb-6 sm:inline-block">
+              Reporte de Ordenes
+            </header>
+            <DataPrincipalOrdenes />
+          </div>
+        </article>
+        <CardsOrdenes />
+      </section>
+    </main>
+  );
 }
-
