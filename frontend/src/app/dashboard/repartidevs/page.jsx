b1c49b58fbee482ff06536aab4e05e62503acdaf
Ricardo2930 <rhuaytan@gmail.com>
2024-07-30 16:28:20 -0500

    feat: :sparkles: Se creo el componente DataRankingRepartidev para ser agregado a la pagina de repartidevs

--- frontend/src/app/dashboard/repartidevs/page.jsx
+++ frontend/src/app/dashboard/repartidevs/page.jsx
@@ -5,6 +5,12 @@ import React from 'react'
 export default function pageRepartidevs() {
   return (
     <main className="mt-28 p-4 sm:p-6 bg-slate-300">
+      <section className="bg-white rounded-xl mb-4 p-5">
+        <header className="flex justify-center sm:justify-start text-3xl font-bold text-gray-900 border-b-4 border-yellow-200 mb-6 sm:inline-block">
+          Ranking
+        </header>
+        
+      </section>
       <section className="flex flex-col gap-5 lg:grid 2xl:grid-cols-7">
         <article className="bg-white p-5 rounded-xl lg:grid 2xl:col-span-6">
           <div>
---
+++ frontend/src/components/dashboard/tablas/DataRankingRepartidev.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function DataRankingRepartidev() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
