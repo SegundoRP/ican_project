11fb99c66ec6a85ae85b7decf07452230603b516
Ricardo2930 <rhuaytan@gmail.com>
2024-07-30 13:42:20 -0500

    feat: :sparkles: Se creo dos componentes para generar nuevas ordenes

---
+++ frontend/src/components/dashboard/Forms/FormNuevaOrden.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FormNuevaOrden() {
+  return (
+    <div>
+      Formulario de Nueva Orden
+    </div>
+  )
+}
---
+++ frontend/src/components/dashboard/modals/NuevaOrden.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NuevaOrden() {
+  return (
+    <div>
+      Generar Nueva Orden
+    </div>
+  )
+}
