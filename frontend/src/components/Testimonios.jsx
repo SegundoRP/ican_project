8035e5c397ecd09b33792a59bb082bd75a436d37
Ricardo2930 <rhuaytan@gmail.com>
2024-10-11 14:13:51 -0500

    feat: :sparkles: updated testimonies component

--- frontend/src/components/Testimonios.jsx
+++ frontend/src/components/Testimonios.jsx
@@ -1,4 +1,23 @@
 export default function Testimonios() {
+
+    const customers = [
+      {
+        "name":"Alvaro Rebaza",
+        "avatar":"https://unavatar.io/segundorp",
+        "testimony":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facererepellendus provident, asperiores omnis suscipit vel accusantiu fuga autem maiores, corporis deserunt mollitia? Et iure excepturialiquam dolor minima minus accusamus."
+      },
+      {
+        "name":"Ricardo Huaytan",
+        "avatar":"https://unavatar.io/ricardo2930",
+        "testimony":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facererepellendus provident, asperiores omnis suscipit vel accusantiu fuga autem maiores, corporis deserunt mollitia? Et iure excepturialiquam dolor minima minus accusamus."
+      },
+      {
+        "name":"Steven Fuertes",
+        "avatar":"https://unavatar.io/midudev",
+        "testimony":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Facererepellendus provident, asperiores omnis suscipit vel accusantiu fuga autem maiores, corporis deserunt mollitia? Et iure excepturialiquam dolor minima minus accusamus."
+      }
+    ]
+
     return (
       <section className="text-center px-4 py-12 sm:my-20 sm:text-center">
         <h2 className="text-5xl md:text-6xl 2xl:text-7xl font-bold text-gray-900">
@@ -8,35 +27,21 @@ export default function Testimonios() {
           que dicen de nosotros
         </h3>
         <div className="grid text-center text-gray-500 md:grid-cols-3 justify-between p-4 items-center gap-3 lg:gap-7 xl:gap-10 lg:mx-20">
-          <div className="border-dotted border-2 border-yellow-300 rounded-xl p-5">
-            <p className="text-pretty py-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-              repellendus provident, asperiores omnis, suscipit vel accusantium
-              fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
-              aliquam dolor minima minus accusamus.
-            </p>
-            <h5 className="sm:p-5 font-semibold">Alvaro Rebaza</h5>
+
+          {
+            customers.map((e) => 
+              
+            <div className="border-dotted border-2 border-yellow-300 rounded-xl p-5">
+              <p className="text-pretty py-5">
+                {e.testimony}
+              </p>
+              <picture className="grid place-content-center">
+                <img src={e.avatar} className="rounded-full w-16"></img>
+              </picture>
+              <h2 className="sm:p-5 font-semibold">{e.name}</h2>
           </div>
-  
-          <div className="border-dotted border-2 border-yellow-300 rounded-xl p-5">
-            <p className="text-pretty py-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-              repellendus provident, asperiores omnis, suscipit vel accusantium
-              fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
-              aliquam dolor minima minus accusamus.
-            </p>
-            <h5 className="sm:p-5 font-semibold">Steven Fuertes</h5>
-          </div>
-  
-          <div className="border-dotted border-2 border-yellow-300 rounded-xl p-5">
-            <p className="text-pretty py-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-              repellendus provident, asperiores omnis, suscipit vel accusantium
-              fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
-              aliquam dolor minima minus accusamus.
-            </p>
-            <h5 className="sm:p-5 font-semibold">Ricardo Huaytan</h5>
-          </div>
+            )
+          }
         </div>
       </section>
     );
