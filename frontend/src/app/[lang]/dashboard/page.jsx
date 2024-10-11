8dfe8c9c17bd896c36b90c0988d2905fb1564f73
Ricardo2930 <rhuaytan@gmail.com>
2024-10-11 16:21:01 -0500

    feat: :sparkles: updated welcome component multi languages in Dashboard Page Home

--- frontend/src/app/[lang]/dashboard/page.jsx
+++ frontend/src/app/[lang]/dashboard/page.jsx
@@ -3,10 +3,16 @@ import Cards from "@/components/dashboard/cards/Cards";
 import DataPrincipal from "@/components/dashboard/DataPrincipal";
 
 
-export default function pageDashboard() {
+export default async function pageDashboard({params}) {
+
+  const {lang} = params
+  const dictionary = await import (`@/app/dictionaries/${lang}.json`)
+  .then((m) => m.default)
+  const dictionaryDashboard = dictionary.DashboardPage
+
   return (
     <main className="p-4 bg-slate-300">
-      <Bienvenido/>
+      <Bienvenido dictDashboard={dictionaryDashboard}/>
       <Cards/>
       <DataPrincipal/>
     </main>
