f0a3f3e39c60b4e37fc3786c7e0fcd116864c3ce
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-12-28 23:06:37 -0500

    feat: add register page

--- frontend/src/app/[lang]/auth/register/page.jsx
+++ frontend/src/app/[lang]/auth/register/page.jsx
@@ -1,12 +1,144 @@
-export default async function pageRegister({params}) {
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image'
+
+export default function PageRegister({params}) {
 
   const {lang} = params
-  const dictionary = await import (`@/app/dictionaries/${lang}.json`)
+  const dictionary = import (`@/app/dictionaries/${lang}.json`)
   .then((m) => m.default)
 
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    re_password: '',
+  });
+
+  const { first_name, last_name, email, password, re_password } = formData;
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
   return (
-    <main>
-      <h1>Register</h1>
-    </main>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <Image
+            alt="Your Company"
+            src=""
+            className="mx-auto h-10 w-auto"
+            width={40}
+            height={40}
+          />
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Sign up to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6">
+            <div>
+              <label htmlFor="first_name" className="block text-sm/6 font-medium text-gray-900">
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first_name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  name="first_name"
+                  type="first_name"
+                  value={first_name}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="last_name" className="block text-sm/6 font-medium text-gray-900">
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last_name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  name="last_name"
+                  type="last_name"
+                  value={last_name}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  name="email"
+                  type="email"
+                  value={email}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  name="password"
+                  type="password"
+                  value={password}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="re_password" className="block text-sm/6 font-medium text-gray-900">
+                Confirm Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="re_password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  name="re_password"
+                  type="re_password"
+                  value={re_password}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+      </div>
   );
 }
