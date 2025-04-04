3aaef8b4289a9a8208d4799f907a748009df7585
Segundo Rebaza <alvaro.rp1593@gmail.com>
2025-04-03 23:59:29 -0500

    fix: export the result of function

--- frontend/src/components/utils/Setup.jsx
+++ frontend/src/components/utils/Setup.jsx
@@ -1,7 +1,25 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useAppDispatch } from '@/redux/hooks';
+import { setAuth, finishInitialLoad } from '@/redux/features/authSlice';
+import { useVerifyMutation } from '@/redux/features/authApiSlice';
 import { ToastContainer } from 'react-toastify';
 
 export default function Setup() {
+  const dispatch = useAppDispatch();
+  const [verify] = useVerifyMutation();
+
+  useEffect(() => {
+    verify(undefined)
+      .unwrap()
+      .then(() => {
+        dispatch(setAuth());
+      })
+      .finally(() => {
+        dispatch(finishInitialLoad());
+      });
+  }, [dispatch, verify]);
+
   return <ToastContainer />;
 }
