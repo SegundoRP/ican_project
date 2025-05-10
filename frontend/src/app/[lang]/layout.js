b38499242534eb7b3dd63f1c4898d269939d98e8
Segundo Rebaza <alvaro.rp1593@gmail.com>
2025-05-09 21:41:14 -0500

    chore: remove navbar from layout

--- frontend/src/app/[lang]/layout.js
+++ frontend/src/app/[lang]/layout.js
@@ -23,7 +23,6 @@ export default async function RootLayout({ children, params }) {
       <body className={inter.className}>
         <Provider>
           <Setup />
-          <Navbar dict={dictionaryLanding} lang={lang}/>
           <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8'>
             {children}
           </div>
