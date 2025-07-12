da27171664bba7f0f2c1193b607cb8c0e68cdbe3
Segundo Rebaza <alvaro.rp1593@gmail.com>
2025-07-12 01:21:10 -0500

    chore: set permissions per user

--- backend-api/users/urls.py
+++ backend-api/users/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
+from rest_framework.routers import DefaultRouter
 from .views import (
     CustomProviderAuthView,
     CustomTokenObtainPairView,
     CustomTokenRefreshView,
     CustomTokenVerifyView,
-    LogoutView
+    LogoutView,
+    UserAccountViewSet
 )
+
+router = DefaultRouter()
+router.register(r'users', UserAccountViewSet)
 
 urlpatterns = [
     re_path(
@@ -16,5 +21,6 @@ urlpatterns = [
     path('jwt/create/', CustomTokenObtainPairView.as_view()),
     path('jwt/refresh/', CustomTokenRefreshView.as_view()),
     path('jwt/verify/', CustomTokenVerifyView.as_view()),
-    path('logout/', LogoutView.as_view())
+    path('logout/', LogoutView.as_view()),
+    path('', include(router.urls)),
 ]
