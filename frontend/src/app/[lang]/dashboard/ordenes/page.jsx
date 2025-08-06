8c69431370d59bdf542c303de8bdf60da2dbcbc9
Segundo Rebaza <alvaro.rp1593@gmail.com>
2025-08-06 00:44:55 -0500

    feat: index orders

--- frontend/src/app/[lang]/dashboard/ordenes/page.jsx
+++ frontend/src/app/[lang]/dashboard/ordenes/page.jsx
@@ -1,27 +1,9 @@
-import CardsOrdenes from "@/components/dashboard/cards/CardsOrdenes";
-import DataPrincipalOrdenes from "@/components/dashboard/tablas/DataPrincipalOrdenes";
-import React from "react";
+import OrdersPageClient from './OrdersPageClient';
 
 export default async function pageOrdenes({params}) {
-
-  const {lang} = params
+  const {lang} = params;
   const dictionary = await import (`@/app/dictionaries/${lang}.json`)
-  .then((m) => m.default)
-  const dictionaryDashboard = dictionary.DashboardPage
+    .then((m) => m.default);
 
-  return (
-    <main className="mt-28 p-4 sm:p-6 bg-slate-300">
-      <section className="flex flex-col gap-5 lg:grid xl:grid-cols-7">
-        <article className="bg-white p-5 rounded-xl lg:grid xl:col-span-6">
-          <div>
-            <header className="flex justify-center sm:justify-start text-3xl font-bold text-gray-900 border-b-4 border-yellow-200 mb-6 sm:inline-block">
-              {dictionaryDashboard.OrdersPage.Title}
-            </header>
-            <DataPrincipalOrdenes dictionary={dictionaryDashboard.Home.DataTablesHome}/>
-          </div>
-        </article>
-        <CardsOrdenes dictionary={dictionaryDashboard.OrdersPage.Cards}/>
-      </section>
-    </main>
-  );
+  return <OrdersPageClient dictionary={dictionary} />;
 }
